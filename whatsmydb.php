340d1365b3644fd6028b8e73ae4f45a50f0e138d
Anthony Rossbach <me@anthonys.space>
2019-01-05 00:33:47 -0800

    Warning added to pages

--- whatsmydb.php
+++ whatsmydb.php
@@ -76,12 +76,182 @@
         max-width:600px;
         padding:10px;
       }
+      warning{
+        display:block;
+        margin: 0px auto;
+        margin-top:20px;
+        text-align:center;
+        max-width:600px;
+        padding:10px;
+        font-size:14px;
+      }
       .block{
   			border-radius: 9px;
   			background:rgb(70,70,70);
         color:rgb(255,255,255);
         padding:10px;
         margin-bottom:10px;
+      }
+
+      textarea {
+      	background: #f7f7f7;
+      	border: 0px solid #f2f2f2;
+      	border: none;
+      	border-radius: 9px;
+      	color: #5a5656;
+      	font-family: inherit;
+      	font-size: 14px;
+      	outline: none;
+      	overflow: hidden;
+      	padding: 10px;
+      	padding-top: 15px;
+      	padding-bottom: 15px;
+      	width: auto;
+      	margin-top: 5px;
+      	margin-bottom: 5px;
+      	-webkit-appearance: none;
+      	box-sizing: border-box;
+      	-moz-box-sizing: border-box;
+      	-webkit-box-sizing: border-box;
+      	resize: none;
+      	-webkit-transition: all 200ms ease-in-out;
+      	-moz-transition: all 200ms ease-in-out;
+      	-o-transition: all 200ms ease-in-out;
+      	transition: all 200ms ease-in-out;
+      }
+  		textarea:hover, textarea:focus {
+  			box-shadow: 0 4px 10px rgba(0, 0, 0, .08);
+  			-webkit-box-shadow: 0 4px 10px rgba(0, 0, 0, .08);
+  			-moz-box-shadow: 0 4px 10px rgba(0, 0, 0, .08);
+  		}
+      input,
+      input[type="text"],
+      input[type="password"] {
+      	background: #e5e5e5;
+      	border: none;
+      	border-radius: 9px;
+      	color: #5a5656;
+      	font-family: inherit;
+      	font-size: 14px;
+      	outline: none;
+      	padding: 10px;
+      	padding-top: 15px;
+      	padding-bottom: 15px;
+      	max-width: 300px;
+      	width: 100%;
+      	margin-top: 5px;
+      	margin-bottom: 5px;
+      	-webkit-appearance: none;
+      	box-sizing: border-box;
+      	-moz-box-sizing: border-box;
+      	-webkit-box-sizing: border-box;
+      	-webkit-transition: all 200ms ease-in-out;
+      	-moz-transition: all 200ms ease-in-out;
+      	-o-transition: all 200ms ease-in-out;
+      	transition: all 200ms ease-in-out;
+      }
+      input:focus,
+  		input:hover,
+      input[type="text"]:focus,
+  		input[type="text"]:hover,
+      input[type="password"]:focus
+  		input[type="password"]:hover {
+      	outline: none;
+  			box-shadow: 0 4px 10px rgba(0, 0, 0, .08);
+  			-webkit-box-shadow: 0 4px 10px rgba(0, 0, 0, .08);
+  			-moz-box-shadow: 0 4px 10px rgba(0, 0, 0, .08);
+      }
+  		input[type=file]{
+  			display: inline-block;
+  		}
+      button,
+      input[type="button"],
+      input[type="submit"] {
+      	background-color: rgb(46, 204, 113);
+      	border: none;
+      	border-radius: 9px;
+      	color: #f4f4f4;
+      	cursor: pointer;
+      	font-family: inherit;
+  			font-weight: 600;
+      	text-transform: uppercase;
+      	outline: none;
+      	padding: 10px;
+      	padding-top: 15px;
+      	padding-bottom: 15px;
+  			width: 100%;
+      	max-width: 300px;
+      	margin-top: 5px;
+      	margin-bottom: 5px;
+      	-webkit-appearance: none;
+      	box-sizing: border-box;
+      	-moz-box-sizing: border-box;
+      	-webkit-box-sizing: border-box;
+      	-webkit-transition: all 200ms ease-in-out;
+      	-moz-transition: all 200ms ease-in-out;
+      	-o-transition: all 200ms ease-in-out;
+      	transition: all 200ms ease-in-out;
+      }
+      button:hover,
+      input[type="button"]:hover,
+      input[type="submit"]:hover {
+      	background-color: rgba(46, 204, 113, 0.8);
+  			box-shadow: 0 4px 10px rgba(0, 0, 0, .08);
+  			-webkit-box-shadow: 0 4px 10px rgba(0, 0, 0, .08);
+  			-moz-box-shadow: 0 4px 10px rgba(0, 0, 0, .08);
+      }
+  		select {
+  			-webkit-appearance: button;
+      	-moz-appearance: button;
+      	background: #e5e5e5;
+      	border: none;
+      	border-radius: 9px;
+      	color: #5a5656;
+      	font-family: inherit;
+      	font-size: 14px;
+      	outline: none;
+      	padding: 10px;
+      	padding-top: 15px;
+      	padding-bottom: 15px;
+      	max-width: 300px;
+      	width: 100%;
+      	margin-top: 5px;
+      	margin-bottom: 5px;
+      	box-sizing: border-box;
+      	-moz-box-sizing: border-box;
+      	-webkit-box-sizing: border-box;
+      	-webkit-transition: all 200ms ease-in-out;
+      	-moz-transition: all 200ms ease-in-out;
+      	-o-transition: all 200ms ease-in-out;
+      	transition: all 200ms ease-in-out;
+      }
+      select:focus
+  		select:hover {
+      	outline: none;
+  			box-shadow: 0 4px 10px rgba(0, 0, 0, .08);
+  			-webkit-box-shadow: 0 4px 10px rgba(0, 0, 0, .08);
+  			-moz-box-shadow: 0 4px 10px rgba(0, 0, 0, .08);
+      }
+      [placeholder]::-webkit-input-placeholder {
+      	color: inherit;
+      }
+      [placeholder]:focus::-webkit-input-placeholder {
+      	-webkit-transition: all 0.3s ease-in-out;
+      	-moz-transition: all 0.3s ease-in-out;
+      	-ms-transition: all 0.3s ease-in-out;
+      	-o-transition: all 0.3s ease-in-out;
+      	transition: all 0.3s ease-in-out;
+      	-webkit-transform: translate(300px, 0);
+      	-moz-transform: translate(300px, 0);
+      	-ms-transform: translate(300px, 0);
+      	-o-transform: translate(300px, 0);
+      	transform: translate(300px, 0);
+      	opacity: 0;
+      }
+      textarea:focus {
+      	outline: none;
+      	border: 0px solid #4D9BD6 !important;
+  			font-weight: 600;
       }
     </style>
   </head>
@@ -117,6 +287,6 @@
 
       ?>
     </content>
-    <credits>Project by Anthony Rossbach on <a href='https://twitter.com/anthonyrossbach'>Twitter</a>, <a href='https://nodehost.cloud/community/view_profile?user=anthony'>NodeHost</a>, and <a href='https://github.com/anthonyrossbach'>Github</a>.</credits>
+    <credits>Project by Anthony Rossbach on <a href='https://twitter.com/anthonyrossbach'>Twitter</a>, <a href='https://nodehost.cloud/community/view_profile?user=anthony'>NodeHost</a>, and <a href='https://github.com/anthonyrossbach'>Github</a>.</credits><warning>Your use of this script is at your sole risk. All code is provided "as -is", without any warranty, whether express or implied, of its accuracy, completeness. Further, I shall not be liable for any damages you may sustain by using this script, whether direct, indirect, special, incidental or consequential.</warning>
   </body>
 </html>
